ab48e888099b4cdf75ec415404d755a98dc245f4
Ksen <k159s632p@gmail.com>
2024-06-10 17:51:15 +0300

    adding new output

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
+# Простой класс для представления книги
 class Book:
-    def init(self, title, author):
+    def init(self, title, author, year):
         self.title = title
         self.author = author
+        self.year = year
 
     def display_info(self):
         print(f"Книга: {self.title}")
         print(f"Автор: {self.author}")
+        print(f"Год выпуска: {self.year}")
 
 # Создание экземпляра книги
-my_book = Book("Война и мир", "Лев Толстой")
+my_book = Book("Война и мир", "Лев Толстой", 1869)
 
 # Вывод информации о книге
 my_book.display_info()
