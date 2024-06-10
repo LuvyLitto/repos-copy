23aa9174d81c40fe2c74f21dd3f5d801e82c8cdb
Ksen <k159s632p@gmail.com>
2024-06-10 17:52:55 +0300

    adding genre

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
+# Простой класс для представления книги
 class Book:
-    def init(self, title, author):
+    def init(self, title, author, year, genre):
         self.title = title
         self.author = author
+        self.year = year
+        self.genre = genre
 
     def display_info(self):
         print(f"Книга: {self.title}")
         print(f"Автор: {self.author}")
+        print(f"Год выпуска: {self.year}")
+        print(f"Жанр: {self.genre}")
 
 # Создание экземпляра книги
-my_book = Book("Война и мир", "Лев Толстой")
+my_book = Book("1984", "Джордж Оруэлл", 1949, "дистопия")
 
 # Вывод информации о книге
-my_book.display_info()
+my_book.display_info())
